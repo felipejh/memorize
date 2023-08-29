84aa5355b68d522ecf16492c923d1d324d442e47
Felipe Hoffmann <felipejh@gmail.com>
2023-08-28 21:32:23 -0300

    first commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Felipe Hoffmann on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
